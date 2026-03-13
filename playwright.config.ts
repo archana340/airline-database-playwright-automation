c0b3037d493849b227d35e46f87ab76c2af0cdb7
Archana Puppireddy <puppireddyarchana@gmail.com>
2026-03-13 00:30:59 -0500

    Start ASP.NET app in CI for Playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,17 +18,15 @@ export default defineConfig({
   },
 
   projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+    { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
+    { name: 'webkit', use: { ...devices['Desktop Safari'] } },
   ],
+
+  webServer: {
+    command: 'dotnet run',
+    url: 'https://localhost:7291',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120000,
+  },
 });
